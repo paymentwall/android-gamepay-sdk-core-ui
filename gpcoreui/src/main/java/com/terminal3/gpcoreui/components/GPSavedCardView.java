3b3edc274c5a01b6574051d6719b8a56df46ee62
Ken Ha <ken@paymentwall.com>
2025-08-04 14:05:13 +0700

    Update SavedCardView

--- gpcoreui/src/main/java/com/terminal3/gpcoreui/components/GPSavedCardView.java
+++ gpcoreui/src/main/java/com/terminal3/gpcoreui/components/GPSavedCardView.java
@@ -2,9 +2,11 @@ package com.terminal3.gpcoreui.components;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.text.InputFilter;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,11 +19,12 @@ import com.terminal3.gpcoreui.enums.SavedCardState;
 
 public class GPSavedCardView extends LinearLayout {
 
+    private LinearLayout llCardRow;
     private ImageView cardBrandView;
     private TextView cardNameView;
     private TextView cardNumberView;
     private ImageView menuView;
-    private GPCardCVVField cvvField;
+    private EditText cvvField;
     private SavedCardState state = SavedCardState.DEFAULT;
 
     public GPSavedCardView(Context context) {
@@ -42,6 +45,7 @@ public class GPSavedCardView extends LinearLayout {
     private void init(Context context) {
         setOrientation(VERTICAL);
         LayoutInflater.from(context).inflate(R.layout.view_saved_card, this, true);
+        llCardRow = findViewById(R.id.gp_card_row);
         cardBrandView = findViewById(R.id.gp_card_brand);
         cardNameView = findViewById(R.id.gp_card_name);
         cardNumberView = findViewById(R.id.gp_card_number);
@@ -49,6 +53,16 @@ public class GPSavedCardView extends LinearLayout {
         cvvField = findViewById(R.id.gp_cvv_field);
         setState(SavedCardState.DEFAULT);
         setOnClickListener(v -> toggle());
+
+        cvvField.setFilters(new InputFilter[] {
+                new InputFilter.LengthFilter(4)
+        });
+        cvvField.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                null,
+                null,
+                ContextCompat.getDrawable(getContext(), R.drawable.ic_card_cvv),
+                null
+        );
     }
 
     public void setState(SavedCardState newState) {
@@ -71,9 +85,11 @@ public class GPSavedCardView extends LinearLayout {
 
     private void updateState() {
         if (state == SavedCardState.SELECTED) {
+            llCardRow.setSelected(true);
             setBackground(ContextCompat.getDrawable(getContext(), R.drawable.gp_saved_card_background_selected));
             cvvField.setVisibility(VISIBLE);
         } else {
+            llCardRow.setSelected(false);
             setBackground(ContextCompat.getDrawable(getContext(), R.drawable.gp_saved_card_background_default));
             cvvField.setVisibility(GONE);
         }
@@ -91,7 +107,7 @@ public class GPSavedCardView extends LinearLayout {
         cardNumberView.setText(number);
     }
 
-    public GPCardCVVField getCvvField() {
+    public EditText getCvvField() {
         return cvvField;
     }
 
